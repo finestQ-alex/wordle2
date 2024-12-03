f35fb17cec9bc7b6099e092c257c039b818a3b0e
finestQ-alex <finestq_alex@icloud.com>
2024-12-03 22:04:31 +0900

    [워들]Javascript 수정
    키보드 클릭시 입력 가능 정답 문자 입력시 키보드에도 색상 변경

--- js/index.js
+++ js/index.js
@@ -12,18 +12,22 @@ function appStart() {
       "display:flex;justify-content:center;align-items:center;position:absolute;top:40vh;left:46vw;background-color:white;width:200px;height:50px;";
     document.body.appendChild(div);
   };
+  //다음줄로 이동
   const nextLine = () => {
     if (attempts === 6) return gameover();
     attempts += 1;
     index = 0;
   };
+  //게임오버
   const gameover = () => {
     window.removeEventListener("keydown", handleKeydown);
     displayGameover();
     clearInterval(timer);
   };
+  //엔터키 입력 처리
   const handleEnterKey = () => {
     let correct_num = 0;
+    // 입력된 글자가 정답이고 위치도 정확하면 초록색 정답이고 위치는 다르면 노란색 오답이면 진한회색
     for (let i = 0; i < 5; i++) {
       const block = document.querySelector(
         `.board-column[data-index='${attempts}${i}']`
@@ -33,15 +37,20 @@ function appStart() {
       if (inputLetter === answerLetter) {
         correct_num += 1;
         block.style.background = "#6aaa64";
-      } else if (answer.includes(inputLetter))
+      } else if (answer.includes(inputLetter)) {
         block.style.background = "#c9b458";
-      else block.style.background = "#787c7e";
+      } else {
+        block.style.background = "#787c7e";
+      }
 
       block.style.color = "white";
+      keyboardColor(inputLetter);
     }
     if (correct_num === 5) gameover();
     else nextLine();
   };
+
+  //백스페이스
   const hadleBackspace = () => {
     if (index > 0) {
       const preBlock = document.querySelector(
@@ -51,12 +60,14 @@ function appStart() {
     }
     if (index !== 0) index -= 1;
   };
+  //키보드 입력
   const handleKeydown = (event) => {
     const key = event.key.toUpperCase();
     const keyCode = event.keyCode;
     const thisBlock = document.querySelector(
       `.board-column[data-index='${attempts}${index}']`
     );
+
     if (event.key === "Backspace") hadleBackspace();
     else if (index === 5) {
       if (event.key === "Enter") handleEnterKey();
@@ -66,6 +77,46 @@ function appStart() {
       index += 1;
     }
   };
+
+  //키보드 색상 업데이트를 위한 버튼 점검
+  const keyboardColor = (inputLetter) => {
+    const keyButton = document.querySelector(`[data-key='${inputLetter}]`);
+    if (!keyButton) return;
+
+    if (answer.includes(inputLetter)) {
+      keyButton.style.background = "#6aaa64";
+      keyButton.style.borderColor = "#6aaa64";
+    } else {
+      keyButton.style.background = "#787c7e";
+      keyButton.style.borderColor = "#787c7e";
+    }
+  };
+
+  //마우스 클릭 이벤트
+  const clickKey = (e) => {
+    const thisBlock = document.querySelector(
+      `.board-column[data-index="${attempts}${index}"]`
+    );
+    const dataKey = e.target.dataset.key;
+
+    if (dataKey === "Backspace") hadleBackspace();
+    else if (index === 5) {
+      if (dataKey === "Enter") handleEnterKey();
+      else return;
+    } else if (dataKey.length < 2) {
+      thisBlock.innerText = e.target.innerText;
+      index += 1;
+
+      keyboardColor(e.target.innerText);
+    }
+  };
+
+  const keyBlocks = document.querySelectorAll(".keyboard-column");
+  for (let keyBlock of keyBlocks) {
+    keyBlock.addEventListener("click", clickKey);
+  }
+
+  //타이머
   const startTimer = () => {
     const start_time = new Date();
 
